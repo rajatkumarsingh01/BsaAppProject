eadc9967c634e4b21101d4300b69934f57c53b53
Rajat Kumar Singh <134076578+rajatkumarsingh01@users.noreply.github.com>
2024-04-17 16:49:20 +0530

    rajat_code - worked on Event form

--- app/src/main/java/com/example/bsagroupproject/screens/EventScreen.kt
+++ app/src/main/java/com/example/bsagroupproject/screens/EventScreen.kt
@@ -1,5 +1,8 @@
 package com.example.bsagroupproject.screens
 
+import android.app.DatePickerDialog
+import android.text.format.DateUtils
+import android.widget.DatePicker
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -19,29 +22,73 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.CalendarToday
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.ModalBottomSheetDefaults
+import androidx.compose.material3.ModalBottomSheetProperties
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.bsagroupproject.R
 import com.example.bsagroupproject.data.PastEvent
+import com.example.bsagroupproject.ui.theme.PurpleGrey80
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.DatePickerDefaults.dateFormatter
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.input.key.Key.Companion.Calendar
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.*
+import kotlin.time.Duration.Companion.days
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EventScreen() {
     val events = listOf(
@@ -50,6 +97,9 @@ fun EventScreen() {
         "1st May 2024" to "Family Picnic",
         "1st June 2024" to "Chabeel on Occasion of Nirjala Ekadshi"
     )
+//check for form
+    val isFormOpen = remember { mutableStateOf(false) }
+    val sheetState = rememberModalBottomSheetState()
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -82,7 +132,7 @@ fun EventScreen() {
 
         FloatingActionButton(
             onClick = {
-                // Handle FAB click here
+                isFormOpen.value = true
             },
             modifier = Modifier
                 .padding(bottom = 40.dp, end = 32.dp)
@@ -94,8 +144,18 @@ fun EventScreen() {
                 tint = Color.Black
             )
         }
+        if (isFormOpen.value) {
+            ModalBottomSheet(
+                onDismissRequest = { isFormOpen.value = false },
+                sheetState = sheetState
+
+            ) {
+                EventForm(isFormOpen = isFormOpen)
+            }
+        }
     }
 }
+
 
 @Composable
 fun EventBanner(date: String, event: String, onClick: () -> Unit) {
@@ -107,14 +167,14 @@ fun EventBanner(date: String, event: String, onClick: () -> Unit) {
             .heightIn(min = 80.dp)
             .background(color = Color(0xFFFFB2D0), RoundedCornerShape(8.dp))
 
-    ){
+    ) {
         Column(
             modifier = Modifier.padding(16.dp),
             verticalArrangement = Arrangement.Center
         ) {
             Text(
                 modifier = Modifier
-                    .background(color=Color(0xFF006400)),
+                    .background(color = Color(0xFF006400)),
                 text = date,
                 style = MaterialTheme.typography.bodyLarge,
                 color = Color.White
@@ -131,7 +191,10 @@ fun EventBanner(date: String, event: String, onClick: () -> Unit) {
 }
 
 @Composable
-fun UpcomingEvents(events: List<Pair<String, String>>, onEventClick: (Pair<String, String>) -> Unit) {
+fun UpcomingEvents(
+    events: List<Pair<String, String>>,
+    onEventClick: (Pair<String, String>) -> Unit
+) {
     LazyRow {
         items(events) { event ->
             EventBanner(date = event.first, event = event.second, onClick = { onEventClick(event) })
@@ -142,8 +205,9 @@ fun UpcomingEvents(events: List<Pair<String, String>>, onEventClick: (Pair<Strin
 
 @Composable
 fun PastEvents() {
-    LazyRow(modifier = Modifier
-        .background(color = Color(0xFFFFB38A))
+    LazyRow(
+        modifier = Modifier
+            .background(color = Color(0xFFFFB38A))
     ) {
         items(pastEvents) { event ->
             EventItem(event)
@@ -186,7 +250,7 @@ fun EventItem(pastevent: PastEvent) {
 val pastEvents = listOf(
     PastEvent(
         title = "Past Event 1",
-        year= "2023"
+        year = "2023"
     ),
     PastEvent(
         title = "Past Event 2",
@@ -206,10 +270,172 @@ val pastEvents = listOf(
     )
 )
 
+@Composable
+fun EventForm(isFormOpen: MutableState<Boolean>) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Text(
+            modifier = Modifier,
+            style = TextStyle(
+                fontSize = 24.sp
+            ),
+            textAlign = TextAlign.Center, text = "Create Event"
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        MyEventTextField(labelValue = "Year & Month")
+        Spacer(modifier = Modifier.height(16.dp))
+        MyEventTextField(labelValue = "Event Name")
+        Spacer(modifier = Modifier.height(16.dp))
+        DateTextField(
+            value = "",
+            onValueChange = {},
+            label = "dd-mm-yyyy",
+            imeAction = ImeAction.Next,
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        MyEventTextField(labelValue = "Event Detail")
+        Spacer(modifier = Modifier.height(16.dp))
+        MyEventTextField(labelValue = "Event Location")
+        Spacer(modifier = Modifier.height(16.dp))
+        MyEventTextField(labelValue = "Lead's Mobile Number")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = { isFormOpen.value = false },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(40.dp)
+        ) {
+            Text(
+                text = "Submit",
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color.White
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyEventTextField(
+    labelValue: String
+) {
+
+    val textValue = remember {
+        mutableStateOf("")
+    }
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(
+                RoundedCornerShape(
+                    topStart = 16.dp,
+                    topEnd = 16.dp,
+                    bottomStart = 16.dp,
+                    bottomEnd = 16.dp
+                )
+            ),
+        label = { Text(text = labelValue) },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = PurpleGrey80,
+            unfocusedBorderColor = PurpleGrey80,
+            cursorColor = PurpleGrey80
+
+        ),
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Default),
+        singleLine = false,
+        maxLines = 5,
+        value = textValue.value,
+        onValueChange = { newValue ->
+            textValue.value = newValue
+        },
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DateTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    imeAction: ImeAction,
+    modifier: Modifier = Modifier,
+    singleLine: Boolean = true
+) {
+    val state = rememberDatePickerState()
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.clickable {
+            }
+        ) {
+            TextField(
+                value = state.selectedDateMillis.toString(),
+                onValueChange = { onValueChange(it) },
+                label = { Text(label) },
+                modifier = Modifier.weight(1f),
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = imeAction),
+                singleLine = singleLine,
+                visualTransformation = VisualTransformation.None,
+                readOnly = true
+            )
+            val showDatePicker = remember {
+                mutableStateOf(false)
+            }
+            IconButton(
+                onClick = {
+                    showDatePicker.value = !showDatePicker.value
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.CalendarToday,
+                    contentDescription = "Select date"
+                )
+            }
+
+            if (showDatePicker.value)
+                DatePickerDialog(
+                    onDismissRequest = { showDatePicker.value = !showDatePicker.value },
+                    confirmButton = {
+                        OutlinedButton(onClick = { showDatePicker.value = !showDatePicker.value }) {
+                            Text(text = "Save")
+                        }
+                    }) {
+                    DatePicker(state = state, dateFormatter = dateFormatter(), headline = {
+                        DatePickerDefaults.DatePickerHeadline(
+                            selectedDateMillis = state.selectedDateMillis,
+                            displayMode = state.displayMode,
+                            dateFormatter = dateFormatter()
+                        )
+                    }
+
+                    )
+
+                }
+        }
+    }
+}
 
 @Preview
 @Composable
-private fun PreviewEventScreen(){
+private fun PreviewDateTextField() {
+    DateTextField(
+        value = "",
+        onValueChange = {},
+        label = "Date",
+        imeAction = ImeAction.Done
+    )
+}
+
+@Preview
+@Composable
+private fun PreviewEventScreen() {
     EventScreen()
 
 }
+
